32787ea8e36e3cc2f2c1532800176218b2e89a0c
ApurvaZun <apu.zunzunwala@gmail.com>
2020-07-25 11:52:49 +0200

    change server.js

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 const express = require("express");
 const path = require("path");
-const port = process.env.PORT || 8080;
 const app = express();
 
 // the __dirname is the current directory from where the script is running
@@ -11,4 +10,7 @@ app.get("*", (req, res) => {
   res.sendFile(path.resolve(__dirname, "index.html"));
 });
 
-app.listen(port);
+var server = app.listen(process.env.PORT || 5000, function() {
+  var port = server.address().port;
+  console.log("Express is working on port " + port);
+});
